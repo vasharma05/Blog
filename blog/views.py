11ec998bcde3baa0d406d7fe5166f6d9f99d3394
vasharma05 <sharmava05@gmail.com>
2020-01-12 00:24:28 +0530

    Bugs Fixed!

--- blog/views.py
+++ blog/views.py
@@ -9,7 +9,7 @@ from django.views.generic import TemplateView, ListView, DetailView, CreateView,
 # Create your views here.
 
 class AboutView(TemplateView):
-    template_name = 'about.html'
+    template_name = 'blog/about.html'
 
 class PostListView(ListView):
     model = Post
