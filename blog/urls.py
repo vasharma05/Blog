86106dc15fccd8e4a144a02cb537d92bf829b52d
vasharma05 <sharmava05@gmail.com>
2020-01-11 14:19:14 +0530

    All the views made

--- blog/urls.py
+++ blog/urls.py
@@ -10,5 +10,7 @@ urlpatterns = [
     path('about/', views.AboutView.as_view(), name='about'),
     path('post/<int:pk>/', views.PostDetailView.as_view(), name='post_detail'),
     path('post/new/', views.CreatePostView.as_view(), name='post_new'),
-    path('post/<int:pk>/edit/', views.UpdatePostView.as_view(), name = 'post_edit')
+    path('post/<int:pk>/edit/', views.UpdatePostView.as_view(), name = 'post_edit'),
+    path('post/<int:pk>/remove/', views.DeletePostView.as_view(), name='post_remove'),
+    path('drafts/', views.DraftListView.as_view(), name='post_draft_list'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render
 from blog.models import Post, Comment
 from blog.forms import PostForm, CommentForm
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.views.generic import TemplateView, ListView, CreateView, UpdateView
+from django.urls import reverse_lazy
+from django.views.generic import TemplateView, ListView, CreateView, UpdateView, DeleteView
 # Create your views here.
 
 class AboutView(TemplateView):
@@ -12,7 +13,7 @@ class PostListView(ListView):
     model = Post
 
     def get_queryset(self):
-        return Post.objects.filter(published_date__lte = timezone.now().order_by('-published_date'))
+        return Post.objects.filter(published_date__lte = timezone.now()).order_by('-published_date')
 
 class PostDetailView(DetailView):
     model = Post
@@ -28,3 +29,15 @@ class UpdatePostView(LoginRequiredMixin, UpdateView):
     redirect_field_name = 'blog/post_detail.html'
     form_class = PostForm
     model = Post
+
+class DeletePostView(LoginRequiredMixin, DeleteView):
+    model = Post
+    success_url = reverse_lazy('post_list')
+
+class DraftListView(LoginRequiredMixin, ListView):
+    login_url ='/login/'
+    redirect_field_name = 'blog/post_list.html'
+    model = Post
+
+    def get_queryset(self):
+        return Post.objects.filter(published_date__isnull=True).order_by('created_date')
